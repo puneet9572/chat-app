58fb78a39b278de2e61cd85ee58b7bb598e9f316
puneet9572 <ps9868817565@gmail.com>
2019-04-02 23:31:16 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 
 const publicPath = path.join(__dirname,'../public');
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT||4500, ()=>{
+
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+    
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected');
+        
+    });
+});
+
+server.listen(process.env.PORT||4500, ()=>{
     console.log('server is live');
     
 })
